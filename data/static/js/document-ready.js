02a63e7f5391bd59e8dbb01c2684b5ca7390484f
azbn <i@azbn.ru>
2018-02-13 11:40:03 +0300

    201802131140

--- data/static/js/document-ready.js
+++ data/static/js/document-ready.js
@@ -2,4 +2,20 @@
 
 $(function() {
 	
+	var __body = $(document.body);
+	
+	var __form = __body.find('form.azbn7__json-edit-form');
+	var __prefix = __form.attr('data-prefix') || '/json/';
+	
+	__body.on('keyup.azbn7', '.azbn7__form-action-change-input', null, function(){
+		
+		var input = $(this);
+		var val = input.val();
+		
+		__form.attr('action', __prefix + val);
+		
+	});
+	
+	__body.find('.azbn7__form-action-change-input').trigger('keyup.azbn7');
+	
 });
